68daae89cc30b077e32cf0a6bb2d2e88e8ed2358
박경두 <ruden@baggyeongduui-iMac.local>
2017-07-18 20:17:51 +0900

    create rect, circle and line

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import Chart from './components/d3';
+import * as d3 from 'd3';
 
-class orangeChart extends Chart {
-}
+let canvas = d3.select('body').append('svg').attr('width', 500).attr('height', 500);
+let circle = canvas.append('circle').attr('cx', 300).attr('cy', 300).attr('r', 100).attr('fill', 'red');
 
-const hiChart = new orangeChart();
+let rect = canvas.append('rect').attr('width', 100).attr('height', 50);
+
+let line = canvas.append('line').attr('x1', 100).attr('y1', 300).attr('x2', 200).attr('y2', 200).attr('stroke', "green").attr('stroke-width', 10);
