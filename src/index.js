397dad3ee2f62f1c4d7700c8d87ad827ae2a7a38
park <ruden91@naver.com>
2017-07-19 00:37:06 +0900

    complete pie chart

--- src/index.js
+++ src/index.js
@@ -1,23 +1,41 @@
 import * as d3 from 'd3';
 
-let width = 500;
-let height = 500;
-let canvas =
-  d3.select('body')
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height);
+let data = [10, 50, 80];
+let width = 1500;
+let height = 1500;
+let r = 300;
 
-let group = canvas.append('g').attr('transform', 'translate(100, 100)');
-
-let r = 100;
-let p = Math.PI * 2;
+let color = d3.scaleOrdinal().range(['red', 'blue', 'orange']);
+let canvas = d3.select('body').append('svg').attr('width', width).attr('height', height);
+let group = canvas.append('g').attr('transform', 'translate(300, 300)');
 
 let arc =
   d3.arc()
-    .innerRadius(r - 20)
-    .outerRadius(r)
-    .startAngle(0)
-    .endAngle(p);
+    .innerRadius(200)
+    .outerRadius(r);
 
-group.append('path').attr('d', arc)
+let pie =
+  d3.pie()
+    .value((d) => {
+      return d;
+    })
+
+let arcs =
+  group.selectAll('.arc')
+    .data(pie(data))
+    .enter()
+    .append('g')
+    .attr('class', 'arc');
+
+arcs.append('path').attr('d', arc).attr('fill', (d) => {
+  return color(d.data);
+})
+
+arcs.append('text').attr('transform', (d) => {
+  return `translate(${arc.centroid(d)})`;
+})
+.attr('text-anchor', 'middle')
+.attr('font-size', '1.5em')
+.text((d) => {
+  return d.data;
+})
