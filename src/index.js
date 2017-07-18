fbfd4ec11a74a8140c7a7ca0a09b26785e7dc6ce
박경두 <ruden@baggyeongduui-iMac.local>
2017-07-18 20:39:01 +0900

    create d3 bar chart

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import * as d3 from 'd3';
 
-let canvas = d3.select('body').append('svg').attr('width', 500).attr('height', 500);
-let circle = canvas.append('circle').attr('cx', 300).attr('cy', 300).attr('r', 100).attr('fill', 'red');
+const dataArray = [20, 150, 50, 40, 120, 320];
+let width = 500;
+let height = 500;
+let widthScale = d3.scaleLinear().domain([0, 60]).range([0, width]);
+let color = d3.scaleLinear().domain([0, 130]).range(['red', 'blue'])
+let canvas = d3.select("body").append('svg').attr('width', width).attr('height', height);
 
-let rect = canvas.append('rect').attr('width', 100).attr('height', 50);
-
-let line = canvas.append('line').attr('x1', 100).attr('y1', 300).attr('x2', 200).attr('y2', 200).attr('stroke', "green").attr('stroke-width', 10);
+let line = canvas
+            .selectAll("rect")
+            .data(dataArray)
+            .enter()
+              .append('rect')
+              .attr('width', function(d) {
+                return widthScale(d);
+              })
+              .attr('fill', (d) => {
+                return color(d);
+              })
+              .attr('height', 50)
+              .attr('y', function(d, i) {
+                return i * 80;
+              })
