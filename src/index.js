64c7a56a841a28ff8008a103031b246bac99b828
park <ruden91@naver.com>
2017-07-19 00:22:06 +0900

    create donut chart

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 import * as d3 from 'd3';
 
-const dataArray = [20, 150, 50, 40, 120, 320];
 let width = 500;
 let height = 500;
-let widthScale = d3.scaleLinear().domain([0, 60]).range([0, width]);
-let color = d3.scaleLinear().domain([0, 130]).range(['red', 'blue'])
-let canvas = d3.select("body").append('svg').attr('width', width).attr('height', height);
+let canvas =
+  d3.select('body')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height);
 
-let line = canvas
-            .selectAll("rect")
-            .data(dataArray)
-            .enter()
-              .append('rect')
-              .attr('width', function(d) {
-                return widthScale(d);
-              })
-              .attr('fill', (d) => {
-                return color(d);
-              })
-              .attr('height', 50)
-              .attr('y', function(d, i) {
-                return i * 80;
-              })
+let group = canvas.append('g').attr('transform', 'translate(100, 100)');
+
+let r = 100;
+let p = Math.PI * 2;
+
+let arc =
+  d3.arc()
+    .innerRadius(r - 20)
+    .outerRadius(r)
+    .startAngle(0)
+    .endAngle(p);
+
+group.append('path').attr('d', arc)
