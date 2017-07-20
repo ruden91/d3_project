445d13f56779ba49c5ee56697ce992b998482537
park <park@parkui-MacBook-Pro.local>
2017-07-20 22:49:05 +0900

    transition test

--- src/index.js
+++ src/index.js
@@ -1,41 +1,50 @@
 import * as d3 from 'd3';
+import config from './components/data.json'
+import { times } from 'lodash';
 
-let data = [10, 50, 80];
-let width = 1500;
+let width = 800;
 let height = 1500;
-let r = 300;
 
-let color = d3.scaleOrdinal().range(['red', 'blue', 'orange']);
-let canvas = d3.select('body').append('svg').attr('width', width).attr('height', height);
-let group = canvas.append('g').attr('transform', 'translate(300, 300)');
+let svg = d3.select('body')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height);
 
-let arc =
-  d3.arc()
-    .innerRadius(200)
-    .outerRadius(r);
-
-let pie =
-  d3.pie()
-    .value((d) => {
-      return d;
-    })
-
-let arcs =
-  group.selectAll('.arc')
-    .data(pie(data))
-    .enter()
-    .append('g')
-    .attr('class', 'arc');
-
-arcs.append('path').attr('d', arc).attr('fill', (d) => {
-  return color(d.data);
+let count = 50
+times(10, (i) => {
+  svg.append('circle')
+    .attr('class', 'first__circle-' + i)
+    .attr('cx', 50)
+    .attr('cy', count += 50)
+    .attr('r', 20)
 })
 
-arcs.append('text').attr('transform', (d) => {
-  return `translate(${arc.centroid(d)})`;
-})
-.attr('text-anchor', 'middle')
-.attr('font-size', '1.5em')
-.text((d) => {
-  return d.data;
-})
+d3.select('.first__circle-0')
+  .transition(d3.transition().duration(1000).ease(d3.easeLinear))
+  .style('fill', '#009ac8')
+  .attr('cx', 500)
+
+d3.select('.first__circle-1')
+  .transition(d3.transition().duration(1000).ease(d3.easePolyIn))
+  .style('fill', '#009ac8')
+  .attr('cx', 500)
+
+d3.select('.first__circle-2')
+  .transition(d3.transition().duration(1000).ease(d3.easeElastic))
+  .style('fill', '#009ac8')
+  .attr('cx', 500)
+
+d3.select('.first__circle-3')
+  .transition(d3.transition().duration(1000).ease(d3.easeElastic))
+  .style('fill', '#009ac8')
+  .attr('cx', 500)
+
+d3.select('.first__circle-4')
+  .transition(d3.transition().duration(1000).ease(d3.easeQuadOut))
+  .style('fill', '#009ac8')
+  .attr('cx', 500)
+
+d3.select('.first__circle-5')
+  .transition(d3.transition().duration(1000).ease(d3.easeCubicOut))
+  .style('fill', '#009ac8')
+  .attr('cx', 500)
