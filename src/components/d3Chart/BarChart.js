aa24732f6676d6db28094a22210d28c6547999ce
park <park@parkui-MacBook-Pro.local>
2017-07-21 00:38:35 +0900

    install d3 package on react app

---
+++ src/components/d3Chart/BarChart.js
@@ -0,0 +1,9 @@
+import D3Chart from './D3Chart';
+
+class BarChart extends D3Chart {
+  constructor(props) {
+    super(props)
+  }
+}
+
+export default BarChart;
---
+++ src/components/d3Chart/D3Chart.js
@@ -0,0 +1,9 @@
+import * from 'd3';
+
+class D3Chart {
+  constructor() {
+    console.log('d3 chart');
+  }
+}
+
+export default D3Chart;
