45cbe1efd1afa8321b51faac44b51cb546c878cc
park <park@parkui-MacBook-Pro.local>
2017-07-21 23:36:43 +0900

    install jquery and bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,13 @@ import React, { Component } from 'react';
 import * as d3 from 'd3';
 
 class App extends Component {
-  constructor() {
-    super();
-
-    let svg = d3.select('body')
-      .append('svg')
-      .attr('class', 'react__d3chart')
-      .attr('width', 320)
-      .attr('height', 320)
-      console.log(svg);
-
-    d3.select('svg')
-      .append('circle')
-      .attr('cx', 160)
-      .attr('cy', 160)
-      .attr('r', 50)
-      .style('fill', '#009ac8')
-  }
   render() {
     return (
-      <div>
-        react x d3
+      <div className="container">
+        <MainRangeSelection />
+        <Cards />
+        <MainContainer />
+        <SubContainer />
       </div>
     )
   }
