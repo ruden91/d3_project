ebeb68d5d9904183e511452da34ca368ee322a4d
park <ruden91@naver.com>
2017-07-23 22:30:59 +0900

    create select button component

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: "Please write an essay about your favorite DOM element."
+      value: "4"
     }
   }
   handleChange = (e) => {
@@ -23,8 +23,13 @@ class App extends Component {
     return (
       <form onSubmit={this.handleSubmit}>
       <label>
-      Name:
-      <textarea value={this.state.value} onChange={this.handleChange}/>
+      Pick Me:
+      <select value={this.state.value} onChange={this.handleChange}>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+      </select>
       </label>
       <input type="submit" value="Submit"/>
       </form>
